af868664dfb6615a310b3ec5ce445b29409ab040
Haerin <kim34458747@gmail.com>
2020-11-24 23:29:12 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Data from "./Data";
 import Ranking from "./Ranking";
 
 function App() {
+  const baseUrl = "http://172.30.1.45:5000";
   return (
     <div>
       <Router>
         <Switch>
-          <Route exact path="/ranking" component={Ranking} />
-          <Route exact path="/data" component={Data} />
+          <Route
+            exact
+            path="/ranking"
+            render={() => <Ranking baseUrl={baseUrl} />}
+          />
+          <Route exact path="/data" render={() => <Data baseUrl={baseUrl} />} />
         </Switch>
       </Router>
     </div>
