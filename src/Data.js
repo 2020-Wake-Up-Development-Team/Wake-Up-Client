03048b0fc29fdb6cedbd68bf264eb48ad89b0e07
Haerin <kim34458747@gmail.com>
2020-11-24 23:28:58 +0900

    Data component edit

--- src/Data.js
+++ src/Data.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Chart from "./Chart";
 import Circle from "./Circle";
 import * as S from "./style";
 import axios from "axios";
+import { useLocation } from "react-router-dom";
 
-const Data = () => {
+const Data = (baseUrl) => {
+  let [loading, setLoading] = useState(false);
+  let [chartData, setChart] = useState([]);
+  let [circleData, setCircle] = useState([]);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state != undefined) {
+      setLoading(true);
+      console.log(location.state.id);
+      // axios.get(baseUrl + '/concentration/detail/circle-chart/' + id, config)
+      // .then((res) => {
+      //   console.log(res);
+      //  setCircle(res.data);
+      // })
+      // axios.get(baseUrl + '/concentration/detail/linear-chart/' + id, config)
+      // .then((res) => {
+      //   console.log(res);
+      //   setChart(res.data);
+      // })
+    } else setLoading(false);
+  });
   return (
     <S.mainContainer>
-      <S.contentContainer>
-        <S.profileContainer>
-          <S.profile />
-          <S.inUser>
-            <S.profileName>31홍길동</S.profileName>
-            <S.profileId>hong</S.profileId>
-          </S.inUser>
-        </S.profileContainer>
-        <S.dataContainer>
-          <S.title>name 님의 수업 집중도 통계 자료입니다.</S.title>
-          <Circle />
-          <Chart />
-        </S.dataContainer>
-      </S.contentContainer>
+      {loading ? (
+        <S.contentContainer>
+          <S.profileContainer>
+            <S.profile />
+            <S.inUser>
+              <S.profileName>
+                {location.state.number}
+                {location.state.name}
+              </S.profileName>
+              <S.profileId>{location.state.id}</S.profileId>
+              <S.school>{location.state.school}</S.school>
+            </S.inUser>
+          </S.profileContainer>
+          <S.dataContainer>
+            <S.title>{location.state.name} 님의 수업 집중도 통계 자료</S.title>
+            <Circle />
+            <Chart />
+          </S.dataContainer>
+        </S.contentContainer>
+      ) : (
+        <div></div>
+      )}
     </S.mainContainer>
   );
 };
